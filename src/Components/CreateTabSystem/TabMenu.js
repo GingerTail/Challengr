1ade0728970ec5ff9f532d7148978ad8c65926dc
Luca <sorace.lc@gmail.com>
2019-12-13 17:05:43 +0100

    fixed bug on button and filternav style

--- src/Components/CreateTabSystem/TabMenu.js
+++ src/Components/CreateTabSystem/TabMenu.js
@@ -45,96 +45,96 @@ export const TabMenu = props => {
   };
 
   return (
-    <div className="create-tab-container" id="top">
-      <div className="backUp-btn fixed-bottom m-4">
-        <a href="#top" className="px-2">
-          <FontAwesomeIcon icon={faAngleUp} size="3x" />
-        </a>
+    <>
+      <a href="#top" className="px-2 backUp-btn">
+        <FontAwesomeIcon icon={faAngleUp} size="3x" />
+      </a>
+      <div className="create-tab-container" id="top">
+        <Nav tabs>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: activeTab === "1" })}
+              onClick={() => {
+                toggle("1");
+              }}
+            >
+              Introduction
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: activeTab === "2" })}
+              onClick={() => {
+                toggle("2");
+              }}
+            >
+              Quiz
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: activeTab === "3" })}
+              onClick={() => {
+                toggle("3");
+              }}
+            >
+              Demo
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: activeTab === "4" })}
+              onClick={() => {
+                toggle("4");
+              }}
+            >
+              General Settings
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: activeTab === "5" })}
+              onClick={() => {
+                toggle("5");
+              }}
+            >
+              Summary
+            </NavLink>
+          </NavItem>
+        </Nav>
+        <TabContent activeTab={activeTab}>
+          <TabPane tabId="1">
+            <div className="challenge-nav p-3 mb-3">
+              <h2>Introduction</h2>
+            </div>
+            <IntroductionTab />
+          </TabPane>
+          <TabPane tabId="2">
+            <div className="challenge-nav p-3 mb-3">
+              <h2>Quiz</h2>
+            </div>
+            <QuizTab />
+          </TabPane>
+          <TabPane tabId="3">
+            <div className="challenge-nav p-3 mb-3">
+              <h2>Demo</h2>
+            </div>
+            <DemoTab />
+          </TabPane>
+          <TabPane tabId="4">
+            <div className="challenge-nav p-3 mb-3">
+              <h2>General Settings</h2>
+            </div>
+            <GeneralSettingsTab />
+          </TabPane>
+          <TabPane tabId="5">
+            <div className="challenge-nav p-3 mb-3">
+              <h2>Summary</h2>
+            </div>
+            <SummaryTab />
+          </TabPane>
+        </TabContent>
       </div>
-      <Nav tabs>
-        <NavItem>
-          <NavLink
-            className={classnames({ active: activeTab === "1" })}
-            onClick={() => {
-              toggle("1");
-            }}
-          >
-            Introduction
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink
-            className={classnames({ active: activeTab === "2" })}
-            onClick={() => {
-              toggle("2");
-            }}
-          >
-            Quiz
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink
-            className={classnames({ active: activeTab === "3" })}
-            onClick={() => {
-              toggle("3");
-            }}
-          >
-            Demo
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink
-            className={classnames({ active: activeTab === "4" })}
-            onClick={() => {
-              toggle("4");
-            }}
-          >
-            General Settings
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink
-            className={classnames({ active: activeTab === "5" })}
-            onClick={() => {
-              toggle("5");
-            }}
-          >
-            Summary
-          </NavLink>
-        </NavItem>
-      </Nav>
-      <TabContent activeTab={activeTab}>
-        <TabPane tabId="1">
-          <div className="challenge-nav p-3 mb-3">
-            <h2>Introduction</h2>
-          </div>
-          <IntroductionTab />
-        </TabPane>
-        <TabPane tabId="2">
-          <div className="challenge-nav p-3 mb-3">
-            <h2>Quiz</h2>
-          </div>
-          <QuizTab />
-        </TabPane>
-        <TabPane tabId="3">
-          <div className="challenge-nav p-3 mb-3">
-            <h2>Demo</h2>
-          </div>
-          <DemoTab />
-        </TabPane>
-        <TabPane tabId="4">
-          <div className="challenge-nav p-3 mb-3">
-            <h2>General Settings</h2>
-          </div>
-          <GeneralSettingsTab />
-        </TabPane>
-        <TabPane tabId="5">
-          <div className="challenge-nav p-3 mb-3">
-            <h2>Summary</h2>
-          </div>
-          <SummaryTab />
-        </TabPane>
-      </TabContent>
-    </div>
+    </>
   );
 };
