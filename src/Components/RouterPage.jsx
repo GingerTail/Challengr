129505424d5daa65f16c273dd74708a00b79749d
Luca <sorace.lc@gmail.com>
2019-12-13 09:24:06 +0100

    fixed bugs on merge

--- src/Components/RouterPage.jsx
+++ src/Components/RouterPage.jsx
@@ -3,16 +3,10 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { connect } from "react-redux";
 import Homepage from "./Homepage/Homepage";
 import Navbar from "./Navbar/Navbar";
-import CreateChallenge from "./CreateChallenge/CreateChallenge";
 import { StartChallenge } from "./StartChallenge/StartChallenge";
 import login from "./LogIn/LogIn";
 import FreeUser from "./FreeUser/FreeUser";
-import Quiz from "./CreateChallenge/Quiz/Quiz";
-import GeneralSettings from "./CreateChallenge/GeneralSettings/GeneralSettings";
-import Demo from "./CreateChallenge/Demo/Demo";
-import Summary from "./CreateChallenge/Summary/Summary";
 import MainPage from "./QuizPlay/MainPage";
-import RedirectPage from "./CreateChallenge/RedirectPage/RedirectPage";
 import { TabMenu } from "./CreateTabSystem/TabMenu";
 
 const mapStateToProps = reduxStore => {
@@ -53,20 +47,11 @@ class RouterPage extends Component {
       <Router>
         <Navbar />
         <Route exact path="/" component={Homepage} />
-        <Route exact path="/create/quiz" component={Quiz} />
-        <Route exact path="/create/demo" component={Demo} />
-        <Route
-          exact
-          path="/create/generalsettings"
-          component={GeneralSettings}
-        />
-        <Route exact path="/create/summary" component={Summary} />
         <Route path="/playQuiz/:id" component={MainPage} />
         <Route exact path="/create" component={TabMenu} />
         <Route exact path="/start/:id" component={StartChallenge} />
         <Route exact path="/login" component={login} />
         <Route exact path="/FreeUser" component={FreeUser} />
-        <Route exact path="/redirect" component={RedirectPage} />
       </Router>
     );
   }
